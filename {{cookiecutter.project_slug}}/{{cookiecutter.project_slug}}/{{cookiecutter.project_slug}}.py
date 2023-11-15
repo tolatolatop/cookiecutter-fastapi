6a967e3ae369e4b475e90148e4016c9ff72f91ec
tolatolatop <tolatolatop@gmail.com>
2023-11-15 16:17:58 +0000

    修改main入口为fastapi

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -1,1 +1,18 @@
-"""Main module."""
+import logging
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+logger = logging.getLogger(__name__)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/healthz")
+async def healthz():
+    return {"message": "OK"}
+
